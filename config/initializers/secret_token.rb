e13db68de06424c037506edca627201a3fe0659f
William Hertling <hertling@WillMac.(none)>
2011-07-05 13:31:21 -0700

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peered::Application.config.secret_token = 'b495777ce3954241ea7338008b80c0d704627fa83eb58ae61b65b3c860daa0c846836131741b03f305b2e1d3af34ee955f363b3e76f1aef3a4a436afd08fd16a'
