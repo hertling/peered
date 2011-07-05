3f024e6fe8eab8a190aa5a87a6d01820dbf956cb
Jeff Weiss <jeff.a.weiss@gmail.com>
2011-07-05 15:53:41 -0700

    added a few very simple rspec tests to editor

--- spec/models/editor_spec.rb
+++ spec/models/editor_spec.rb
@@ -1,7 +1,23 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Editor do
-  it "should be valid" do
-    Editor.new.should be_valid
+  before(:each) do
+    @attr = { :name => "Maddie Whitmarsh", :bio => "College Student" }
   end
+
+  it "should create a new instance given valid attributes" do
+    Editor.create!(@attr)
+  end
+
+  it "should require a name" do
+    no_name_editor = Editor.new(@attr.merge(:name => ""))
+    no_name_editor.should_not be_valid
+  end
+
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_editor = Editor.new(@attr.merge(:name => long_name))
+    long_name_editor.should_not be_valid
+  end
+
 end
