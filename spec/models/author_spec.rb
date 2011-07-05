4f6fdab58f6a4d2ff2f8dab1a418826913faea8f
William Hertling <hertling@WillMac.(none)>
2011-07-05 16:25:22 -0700

    author spec

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,4 +4,29 @@ describe Author do
   it "should be valid" do
     Author.new.should be_valid
   end
+
+  describe "success" do
+    before(:each) do
+      @attr={:name =>'Will', :bio=>'RocketScientist'}
+    end
+    
+    it "should have a name" do
+      a = Author.new(@attr)
+      a.name.should == @attr[:name]
+    end
+
+    it "should have a bio" do
+      a = Author.new(@attr)
+      a.bio.should == @attr[:bio]
+    end
+    
+  end
+  
+  describe "failure" do
+    it "should reject an empty name" do
+      Author.new(:name=>"").should_not be_valid
+    end
+  end
 end
+
+
