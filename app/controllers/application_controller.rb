15dbd187d393840e6074d954bd0c218c89e78296
William Hertling <william.hertling@hp.com>
2011-07-07 15:47:28 -0700

    fixed error I created.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
 private
   def current_user
+    session[:user_id]=nil if params[:reset_id]=="true"
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end  
 
